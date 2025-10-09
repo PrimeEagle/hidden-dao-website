5ed1f5ec3610cf08d41a9bffae7cd1524c5f0af4
PrimeEagle <misc@hiddendragon.com>
2025-10-08 23:15:52 -0500

    adjusted Hero section layout

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -36,19 +36,19 @@ function useWiggle(seed = 1000, freq = 0.01, amp = 10) {
 export default function Hero() {
   const cloud1Ref = useWiggle(1234, 0.03, 19);
   const cloud2Ref = useWiggle(1235, 0.04, 20);
-  const cloud3Ref = useWiggle(1236, 0.02, 18);
+  const cloud3Ref = useWiggle(1236, 0.025, 18);
 
   return (
     <Section
       id="hero"
       fullWidth
       noPadding
-      className="h-screen w-screen flex flex-col lg:flex-row overflow-hidden"
+      className="h-screen flex flex-col lg:flex-row justify-center items-center overflow-hidden"
     >
       {/* LEFT */}
-      <div className="flex flex-1 flex-col min-w-[50vw] justify-between items-center px-8">
+      <div className="flex flex-col h-[90vh] justify-center items-center px-8">
         <div>
-          <div className="font-logo text-9xl lg:text-8xl bg-gradient-to-t from-[#372537] to-[#664c66] bg-clip-text text-transparent px-2">
+          <div className="font-logo text-7xl lg:text-8xl bg-gradient-to-t from-[#372537] to-[#664c66] bg-clip-text text-transparent px-2">
             Hidden Dao
           </div>
 
@@ -57,8 +57,8 @@ export default function Hero() {
           </div>
         </div>
 
-        <div className="font-heading text-lg text-brand-secondary text-center leading-relaxed">
-          <p className="italic mb-1 text-brand-starkAccent">
+        <div className="font-heading text-lg text-brand-secondary text-center leading-relaxed pt-6">
+          <p className="italic mb-1 text-brand-accent">
             A different kind of martial arts school.
           </p>
           <p className="mb-1">No gimmicks, no sales pitches, no hidden fees.</p>
@@ -70,7 +70,7 @@ export default function Hero() {
             <span className="font-bold italic">Daoism</span> in a natural
             outdoor setting.
           </p>
-          <div className="flex justify-center mt-4">
+          <div className="flex justify-center pt-2">
             <img
               src="/hidden-dao-seal.png"
               alt="Seal Stamp"
@@ -78,57 +78,55 @@ export default function Hero() {
             />
           </div>
         </div>
-        <div>
-          <div className="flex -translate-y-[100%]">
-            <button
-              type="button"
-              className="mt-6 rounded px-6 py-4 bg-brand-starkAccent text-brand-light text-2xl font-heading hover:opacity-90 transition"
-            >
-              Sign Up Now!
-            </button>
-          </div>
+        <div className="pt-6">
+          <button
+            type="button"
+            className="mt-6 rounded px-6 py-4 bg-brand-starkAccent text-brand-light text-2xl font-heading hover:opacity-90 transition"
+          >
+            Sign Up Now!
+          </button>
         </div>
       </div>
 
       {/* RIGHT */}
-      <div className="relative flex-1 flex justify-center">
-        <div className="flex h-screen min-h-[450px] aspect-square items-center">
+      <div className="relative flex h-[90vh] max-w-[802px] justify-center">
+        <div className="flex w-[80%] aspect-square items-center">
           <img
             src="/yin-yang.png"
             alt="Yin Yang"
             className="object-contain animate-[spin_180s_linear_infinite] z-0"
           />
 
-          {/* <img
+          <img
             ref={cloud1Ref}
             src="/cloud-1.png"
             alt="Cloud 1"
-            className="absolute top-[25%] left-[20%] w-[35%] z-10"
+            className="absolute top-[25%] left-[0%] w-[60%] z-20"
           />
           <img
             ref={cloud2Ref}
             src="/cloud-2.png"
             alt="Cloud 2"
-            className="absolute top-[50%] right-[15%] w-[40%] z-10"
+            className="absolute top-[45%] right-[-5%] w-[60%] z-20"
           />
           <img
             ref={cloud3Ref}
             src="/cloud-3.png"
             alt="Cloud 3"
-            className="absolute bottom-[10%] left-[25%] w-[38%] z-10"
-          /> */}
+            className="absolute bottom-[25%] left-[10%] w-[60%] z-20"
+          />
 
-          {/* <div className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] z-20 [writing-mode:vertical-rl] font-chinese bg-gradient-to-t from-[#3c3c3c] to-[#674d41] bg-clip-text text-transparent text-[8%]">
+          <div className="absolute top-1/2 left-1/2 -translate-x-[125%] -translate-y-[50%] z-10 [writing-mode:vertical-rl] font-chinese bg-gradient-to-t from-[#3c3c3c] to-[#674d41] bg-clip-text text-transparent text-[700%]">
             隱道
           </div>
 
-          <div className="absolute top-1/2 left-[15%] -translate-y-[50%] z-20 [writing-mode:vertical-rl] font-chinese text-[#251a18] text-[5%]">
+          <div className="absolute top-1/2 left-1/2 -translate-x-[580%] -translate-y-[50%] z-10 [writing-mode:vertical-rl] font-chinese text-[#251a18] text-[250%]">
             道隱無名
           </div>
 
-          <div className="absolute top-1/2 right-[15%] -translate-y-[50%] z-20 [writing-mode:vertical-rl] font-chinese text-[#251a18] text-[5%]">
+          <div className="absolute top-1/2 left-1/2  -translate-x-[-420%] -translate-y-[190%] z-10 [writing-mode:vertical-rl] font-chinese text-[#251a18] text-[250%]">
             隱道宗門
-          </div> */}
+          </div>
         </div>
       </div>
     </Section>
